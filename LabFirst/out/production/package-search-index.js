1035c03d47287658c94089fe57e476668f09a4b7
limashou <poliatskyi.maksym@gmail.com>
2023-12-19 01:14:28 +0200

    Laboratory work

--- LabFirst/out/production/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"TaskFive"},{"l":"TaskFour"},{"l":"TaskOne"},{"l":"TaskSix"},{"l":"TaskThree"},{"l":"TaskTwo"}];updateSearchResults();
